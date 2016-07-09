dff1775933caa51aa6281665e3591449f8c41393
pimblottg <g.pimblott@kainos.com>
2016-07-09 19:10:58 +0100

    Added Spring Boot Actuator and Hawt.io to the base project

--- spring-boot-camel/src/main/java/com/pimblott/samples/spring/boot/camel/ExampleCamelApplication.java
+++ spring-boot-camel/src/main/java/com/pimblott/samples/spring/boot/camel/ExampleCamelApplication.java
@@ -1,6 +1,8 @@
 package com.pimblott.samples.spring.boot.camel;
 
 
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.web.AuthenticationFilter;
 import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +11,16 @@ import org.springframework.context.annotation.Bean;
  * Created by Gordon on 19/05/2015.
  */
 @SpringBootApplication
+@EnableHawtio
 public class ExampleCamelApplication extends FatJarRouter {
 
 
     @Override
     public void configure() throws Exception {
+
+        System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+        System.setProperty( "port" , "8092");
+
         from("netty-http:http://0.0.0.0:18080").
                 setBody().simple("ref:helloWorld");
     }
